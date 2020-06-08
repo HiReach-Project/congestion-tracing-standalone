78b5829db39b7c4ea0c8a62016df70dc8ab580fc
codr <codrut.panea@esolutions.ro>
2020-06-08 17:46:51 +0300

    update db schema

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,8 @@
 create table device_location
 (
     id bigint primary key,
-    company_access_key varchar not null,
-    device_id varchar not null,
+    company_access_key varchar(50) not null,
+    device_id varchar(50) not null unique,
     updated_at timestamp default timezone('utc', now()),
     location_point geography(POINT, 4326) not null
 );
