ef1e2b4e5001c181ba19e90353a08c34f790d0d7
codr <codrut.panea@esolutions.ro>
2020-07-27 09:04:03 +0300

    simulate data

--- src/test/java/com/hireach/congestiontracingstandalone/CongestionTracingApplicationTests.java
+++ src/test/java/com/hireach/congestiontracingstandalone/CongestionTracingApplicationTests.java
@@ -1,30 +1,63 @@
 package com.hireach.congestiontracingstandalone;
 
+import com.hireach.congestiontracingstandalone.entity.Company;
+import com.hireach.congestiontracingstandalone.repository.CompanyRepository;
+import com.hireach.congestiontracingstandalone.service.DeviceLocationHistoryService;
 import com.hireach.congestiontracingstandalone.service.DeviceLocationService;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
-import static org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA3_256;
+import java.text.DecimalFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.ThreadLocalRandom;
 
-//@SpringBootTest
+@SpringBootTest
 class CongestionTracingApplicationTests {
 
     @Autowired
     DeviceLocationService deviceLocationService;
 
+    @Autowired
+    DeviceLocationHistoryService deviceLocationHistoryService;
+
+    @Autowired
+    CompanyRepository companyRepository;
+
     @Test
     @Rollback(value = false)
-    void contextLoads() {
-        String sha3_256hex = new DigestUtils(SHA3_256).digestAsHex("HM4fQAQmLF36LCWEctCuJrEtqTtgegpG");
-        System.out.println(sha3_256hex);
-//        Instant instant = Instant.now();
-//        System.out.println(instant);
-//        // bucharest
-//        //deviceLocationService.saveTrace(44.426164962 , 26.102332924, "sx", "zz");
-//        // cluj
-//        deviceLocationService.saveOrUpdateDeviceLocation(46.7666700, 23.6000000, "sx", "zz", Instant.now());
+    void simulateDeviceLocations() {
+        Company testCompany = companyRepository.findAll().get(0);
+        DecimalFormat df = new DecimalFormat("##.######");
+        // stanga sus 44.480239, 26.027606
+        // dreapta sus 44.480239, 26.148456
+        // dreapta jos 44.397630, 26.148456
+        // stanga jos 44.397630, 26.027606
+        Instant start = Instant.now();
+
+        Instant tempTime = Instant.now().minus(2, ChronoUnit.DAYS);
+
+        while (tempTime.isBefore(Instant.now())) {
+            for (int i = 0; i < 1000; i++) {
+                double randomLat = ThreadLocalRandom.current().nextDouble(44.397630, 44.480239);
+                double randomLon = ThreadLocalRandom.current().nextDouble(26.027606, 26.148456);
+
+                deviceLocationHistoryService.saveDeviceLocationHistory(
+                        Double.parseDouble(df.format(randomLat)),
+                        Double.parseDouble(df.format(randomLon)),
+                        "deviceId" + i,
+                        testCompany,
+                        tempTime
+                );
+            }
+            tempTime = tempTime.plusSeconds(30);
+        }
+
+        System.out.println(Duration.between(start, Instant.now()));
+
     }
 
 }
