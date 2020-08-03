2f79df4c0a84d2855954cfea84c2f8d2ec6e2d93
codr <codrut.panea@esolutions.ro>
2020-08-03 09:12:28 +0300

    testing device location simulation

--- src/main/java/com/hireach/congestiontracingstandalone/repository/DeviceLocationHistoryRepository.java
+++ src/main/java/com/hireach/congestiontracingstandalone/repository/DeviceLocationHistoryRepository.java
@@ -1,9 +1,15 @@
 package com.hireach.congestiontracingstandalone.repository;
 
 import com.hireach.congestiontracingstandalone.entity.DeviceLocationHistory;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeviceLocationHistoryRepository extends JpaRepository<DeviceLocationHistory, Long> {
+    @Query(value = "SELECT count(*)\n" +
+            "FROM device_location_history\n" +
+            "WHERE ST_DistanceSphere(geometry(location_point), ?1) < ?2", nativeQuery = true)
+    int getHistory(Point point, Double radius);
 }
--- src/test/java/com/hireach/congestiontracingstandalone/CongestionTracingApplicationTests.java
+++ src/test/java/com/hireach/congestiontracingstandalone/CongestionTracingApplicationTests.java
@@ -2,9 +2,11 @@ package com.hireach.congestiontracingstandalone;
 
 import com.hireach.congestiontracingstandalone.entity.Company;
 import com.hireach.congestiontracingstandalone.repository.CompanyRepository;
+import com.hireach.congestiontracingstandalone.repository.DeviceLocationHistoryRepository;
 import com.hireach.congestiontracingstandalone.service.DeviceLocationHistoryService;
 import com.hireach.congestiontracingstandalone.service.DeviceLocationService;
 import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.Point;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
@@ -14,6 +16,8 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.concurrent.ThreadLocalRandom;
+
+import static com.hireach.congestiontracingstandalone.util.Util.createPoint;
 
 @SpringBootTest
 class CongestionTracingApplicationTests {
@@ -26,6 +30,21 @@ class CongestionTracingApplicationTests {
 
     @Autowired
     CompanyRepository companyRepository;
+
+    @Autowired
+    DeviceLocationHistoryRepository deviceLocationHistoryRepository;
+
+    @Test
+    public void getHistory() {
+        Instant start = Instant.now();
+
+        Point point = createPoint(44.4133671, 26.1630280);
+        int total = deviceLocationHistoryRepository.getHistory(point, 1000D);
+        System.out.println(Duration.between(start, Instant.now()));
+
+        System.out.println(total);
+
+    }
 
     @Test
     @Rollback(value = false)
@@ -58,6 +77,45 @@ class CongestionTracingApplicationTests {
 
         System.out.println(Duration.between(start, Instant.now()));
 
+    }
+
+    @Test
+    @Rollback(value = false)
+    void simulateDeviceLocationsInBusStops() {
+        Company testCompany = companyRepository.findAll().get(0);
+        Point point = createPoint(44.4133000, 26.1630000);
+
+        Instant start = Instant.now();
+
+        Instant tempTime = Instant.now().minus(2, ChronoUnit.DAYS);
+
+        while (tempTime.isBefore(Instant.now())) {
+            // if time is between 7-9 or 16-20 atunci 10-15 oameni
+            // else if 9-16 atunci 5-8
+            // else if 20-23 random 0-4
+            // else random 0-1
+            createPoints(testCompany, tempTime);
+            tempTime = tempTime.plusSeconds(30);
+        }
+
+        System.out.println(Duration.between(start, Instant.now()));
+
+    }
+
+    private void createPoints(Company testCompany, Instant tempTime) {
+        DecimalFormat df = new DecimalFormat("##.#######");
+        for (int i = 0; i < 1000; i++) {
+            double randomLat = ThreadLocalRandom.current().nextDouble(44.4133000, 44.4134000);
+            double randomLon = ThreadLocalRandom.current().nextDouble(26.1630000, 26.1631000);
+
+            deviceLocationHistoryService.saveDeviceLocationHistory(
+                    Double.parseDouble(df.format(randomLat)),
+                    Double.parseDouble(df.format(randomLon)),
+                    "deviceId" + i,
+                    testCompany,
+                    tempTime
+            );
+        }
     }
 
 }
