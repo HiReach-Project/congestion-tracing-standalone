b6a3ea3f0be3e143ad2aed4553ec54b59991fdf3
codr <codrut.panea@esolutions.ro>
2020-08-15 00:45:06 +0300

    add sql query for getting the history to predict the future

--- src/main/java/com/hireach/congestiontracingstandalone/repository/DeviceLocationHistoryRepository.java
+++ src/main/java/com/hireach/congestiontracingstandalone/repository/DeviceLocationHistoryRepository.java
@@ -8,8 +8,20 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeviceLocationHistoryRepository extends JpaRepository<DeviceLocationHistory, Long> {
-    @Query(value = "SELECT count(*)\n" +
+    @Query(value = "select rounded_date,  count(distinct device_id) from\n" +
+            "(SELECT id,\n" +
+            "       company_id,\n" +
+            "       device_id,\n" +
+            "       location_point,\n" +
+            "       case\n" +
+            "           when extract(SECONDS from updated_at) <= 30\n" +
+            "               then (date_trunc('minute', updated_at) + interval '30 seconds')\n" +
+            "           when extract(SECONDS from updated_at) > 30\n" +
+            "               then date_trunc('minute', updated_at + interval '30 seconds')\n" +
+            "           end rounded_date\n" +
             "FROM device_location_history\n" +
-            "WHERE ST_DistanceSphere(geometry(location_point), ?1) < ?2", nativeQuery = true)
+            "WHERE ST_DistanceSphere(geometry(location_point), ?1) < ?2\n" +
+            "    ) as a\n" +
+            "group by rounded_date", nativeQuery = true)
     int getHistory(Point point, Double radius);
 }
