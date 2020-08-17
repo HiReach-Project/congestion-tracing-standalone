23f0ba0158ac478ddc461739f0a04b121ae74378
codr <codrut.panea@esolutions.ro>
2020-08-17 09:35:32 +0300

    update getHistory query

--- src/main/java/com/hireach/congestiontracingstandalone/repository/DeviceLocationHistoryRepository.java
+++ src/main/java/com/hireach/congestiontracingstandalone/repository/DeviceLocationHistoryRepository.java
@@ -17,17 +17,17 @@ public interface DeviceLocationHistoryRepository extends JpaRepository<DeviceLoc
          Integer getDevices();
     }
 
-    @Query(value = "SELECT date_rounded, count(DISTINCT device_id) AS devices\n" +
+    @Query(value = "SELECT dateRounded, count(DISTINCT device_id) AS devices\n" +
             "FROM (SELECT device_id,\n" +
             "             CASE\n" +
             "                 WHEN extract(SECONDS FROM updated_at) <= 30\n" +
             "                     THEN (date_trunc('minute', updated_at) + INTERVAL '30 seconds')\n" +
             "                 WHEN extract(SECONDS FROM updated_at) > 30\n" +
             "                     THEN date_trunc('minute', updated_at + INTERVAL '30 seconds')\n" +
-            "                 END AS date_rounded\n" +
+            "                 END AS dateRounded\n" +
             "      FROM device_location_history\n" +
             "      WHERE ST_DistanceSphere(geometry(location_point), ?1) < ?2\n" +
             "     ) AS a\n" +
-            "GROUP BY date_rounded;", nativeQuery = true)
+            "GROUP BY dateRounded;", nativeQuery = true)
     List<MLDataModel> getHistory(Point point, Double radius);
 }
